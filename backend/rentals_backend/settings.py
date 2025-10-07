b0f80b3569dc71ee496fef35a30d081237ffc2c4
jmwa200 <jmwa200@gmail.com>
2025-10-07 16:57:33 +0300

    adding backend 12

--- backend/rentals_backend/settings.py
+++ backend/rentals_backend/settings.py
@@ -57,10 +57,15 @@ WSGI_APPLICATION = 'rentals_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME', default='railway'),
+        'USER': config('DB_USER', default='postgres'),
+        'PASSWORD': config('DB_PASSWORD', default='password'),
+        'HOST': config('DB_HOST', default='localhost'),
+        'PORT': config('DB_PORT', default='5432'),
     }
 }
+
 
 
 AUTH_PASSWORD_VALIDATORS = [
